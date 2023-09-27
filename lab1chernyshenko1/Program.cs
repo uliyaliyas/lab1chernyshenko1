de9f547e37cb31e85cd804a42a20ab64b0586f95
5090969 <5090969@LAPTOP-NB62C61V>
2023-09-27 20:45:41 +0200

    Добавьте файлы проекта.

---
+++ lab1chernyshenko1/Program.cs
@@ -0,0 +1,11 @@
+﻿высокий уровень
+Console.Write("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите z:");
+double z = double.Parse(Console.ReadLine());
+double f = (Math.Cos(7) * b * Math.Pow(x, 5) - (Math.Sin(a * a) + Math.Cos((x * x * x) + Math.Pow(z, 5) - (a * a)))) / (Math.Asin(a * a) + Math.Acos(Math.Pow(x, 7) - (a * a)));
+Console.WriteLine($"f={f:F2}");
